b11c03884e0ffe8b3f6bff07c09610f4db7e9da2
videv93 <videv93@gmail.com>
2024-04-10 09:35:43 +0700

    Refactor code structure and remove unused files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
@@ -46,7 +44,5 @@ import { RedisClientOptions } from 'redis';
     ConfigModule.forRoot(),
     EventsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
