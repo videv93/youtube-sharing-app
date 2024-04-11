b44acfc3b8eb94646c2489d08345c756f5538223
videv93 <videv93@gmail.com>
2024-04-11 15:35:17 +0700

    Add Jest import statement in setupTests.ts for unit testing

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -2,6 +2,7 @@ import { afterEach, beforeAll, afterAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import { mockServer } from "./mockServer";
 import "@testing-library/jest-dom/vitest";
+import "jest";
 
 beforeAll(() => {
   mockServer.listen({ onUnhandledRequest: "error" });
